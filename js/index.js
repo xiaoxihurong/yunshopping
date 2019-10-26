7e4d2fde5ed61046839b0a6a68dcfcf2121400e0
xiaoxihurong <1120407159@qq.com>
2019-10-26 21:30:08 +0800

    怀化面试页面 第一次提交

---
+++ js/index.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    //顶部导航
+    $(".nav-uu li").on("mouseenter", function() {
+        $(this).addClass("nav-mouse").siblings().removeClass("nav-mouse");
+    });
+    $(".nav-uu").on("mouseleave", function() {
+        $(this).children().removeClass("nav-mouse");
+        $(this).children("li a:eq(0)").addClass("nav-mouse");
+    });
+
+    //底部导航
+    $(".bottom-uu li").on('click',function(){
+        $(this).addClass('bottom-black').siblings().removeClass('bottom-black');
+    });
+
+
+
+
+    //轮播图    
+    var count = 0;
+    var timeId;
+    timeId = setInterval(clickHandle, 2000);
+    $(".arrow-right").click(function() {
+        count++;
+        if (count == $(".slider li").length) {
+            count = 0;
+        }
+        $(".slider li").eq(count).fadeIn().siblings("li").fadeOut();
+    });
+
+    $(".arrow-left").click(function() {
+        count--;
+
+        if (count == -1) {
+            count = $(".slider li").length - 1;
+        }
+        $(".slider li").eq(count).fadeIn().siblings("li").fadeOut();
+    });
+    $(".slider").on('mouseenter',function(){
+        $(".arrow").css("display","block");
+        clearInterval(timeId);
+    }).on('mouseleave',function(){
+        $(".arrow").css("display","none");
+        timeId = setInterval(clickHandle, 2000);
+    })   
+    function clickHandle() {
+        count++;
+        if (count == $(".slider li").length) {
+            count = 0;
+        }
+        $(".slider li").eq(count).fadeIn().siblings("li").fadeOut();
+    };
+
+});
